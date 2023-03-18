4d630b4731b2a4073c66e57bca76383c8df9f820
Neil <neilkhess@protonmail.com>
2023-03-18 12:13:40 -0700

    Began making book prototypes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
+function book(title, author, pages, status) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
+}
 
+book.prototype.getTitle = function() {
+console.log(this.title)
+}
+
+book.prototype.getAuthor = function() {
+    console.log(this.author)
+}
+
+book.prototype.getPages= function() {
+    console.log(this.pages)
+}
+
+book.prototype.getStatus = function() {
+    console.log(this.status)
+}
