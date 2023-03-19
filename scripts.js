5a33f7380bb82d9f432e4963d8e1ddac6e6c52f1
Neil Hess <neilkhess@protonmail.com>
2023-03-19 10:26:34 -0700

    Began constructing table

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function book(title, author, pages, status) {
     this.title = title
     this.author = author
@@ -5,8 +7,20 @@ function book(title, author, pages, status) {
     this.status = status
 }
 
+function addBookToLibrary(){
+
+}
+
+function displayAllBooks(){
+    let i = 0;
+    while (i < myLibrary.length){
+        console.log(myLibrary[i]);
+        i++;
+    }
+}
+
 book.prototype.getTitle = function() {
-console.log(this.title)
+    console.log(this.title)
 }
 
 book.prototype.getAuthor = function() {
